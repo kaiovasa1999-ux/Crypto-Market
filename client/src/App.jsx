333a981e22ae3cda3b585289bf3a13753e21e5f1
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-12-28 12:16:04 +0200

    initial create

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
